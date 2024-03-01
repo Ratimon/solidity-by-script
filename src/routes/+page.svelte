337595fd88162a8256e17079d52c5f92ea254118
Ratimon <ratixoxo@gmail.com>
2024-03-01 08:29:27 +0700

    Feat & Refactor (./src/): Adding Copied and Check button && Using daisyui component instead of css styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,22 @@
 <script  lang="ts">
 
+    import { createEventDispatcher } from 'svelte';
+
+    import hljs from './highlightjs';
+
     import Header from './Header.svelte';
     import OverflowMenu from '$lib/ui/components/OverflowMenu.svelte';
+    import CopyIcon from '$lib/ui/icons/CopyIcon.svelte';
+    import CheckIcon from '$lib/ui/icons/CheckIcon.svelte';
+    // import Footer from './Footer.svelte';
+
     import type { KindedOptions, Kind, Contract, OptionsErrorMessages } from '$lib/wizard';
     import { ContractBuilder, buildGeneric, printContract, sanitizeKind, OptionsError } from '$lib/wizard';
-    // import Footer from './Footer.svelte';
+    import { postConfig } from './post-config';
+
+    import { injectHyperlinks } from '$lib/ui/utils/inject-hyperlinks';
+
+    
 
     // in case you want to use the multiple pages app 
     const headLinks = [
@@ -21,52 +33,185 @@
     //     {pathname: '/twitter', title: 'Twitter'},
   	// ];
 
+    const dispatch = createEventDispatcher();
+
     export let initialTab: string | undefined = 'ERC20';
     export let tab: Kind = sanitizeKind(initialTab);
+
+    $: {
+      tab = sanitizeKind(tab);
+      dispatch('tab-change', tab);
+    };
+
+    let allOpts: { [k in Kind]?: Required<KindedOptions[k]> } = {};
+    let errors: { [k in Kind]?: OptionsErrorMessages } = {};
+
+    let contract: Contract = new ContractBuilder('MyToken');
+
+    $: opts = allOpts[tab];
+
+    $: {
+      if (opts) {
+        try {
+          contract = buildGeneric(opts);
+          errors[tab] = undefined;
+        } catch (e: unknown) {
+          if (e instanceof OptionsError) {
+            errors[tab] = e.messages;
+          } else {
+            throw e;
+          }
+        }
+      }
+    }
+
+$: code = printContract(contract);
+$: highlightedCode = injectHyperlinks(hljs.highlight('solidity', code).value);
+
+const language = 'solidity';
+
+
+    let copied = false;
+    const copyHandler = async () => {
+      await navigator.clipboard.writeText(code);
+      copied = true;
+      if (opts) {
+        await postConfig(opts, 'copy', language);
+      }
+      setTimeout(() => {
+        copied = false;
+      }, 1000);
+    };
     
   
   </script>
     
   <Header links={headLinks} class="bg-base-200 "></Header>
 
-  <div class="container flex flex-col gap-4 p-4">
+  <!-- <div class="container flex flex-col gap-4 p-4"> -->
 
-    <div class="header flex flex-row justify-between">
-        <div class="tab overflow-hidden">
-            <OverflowMenu>
-              <button class:selected={tab === 'ERC20'} on:click={() => tab = 'ERC20'}>
-                ERC20
-              </button>
-              <button class:selected={tab === 'ERC721'} on:click={() => tab = 'ERC721'}>
-                ERC721
-              </button>
-              <button class:selected={tab === 'ERC1155'} on:click={() => tab = 'ERC1155'}>
-                ERC1155
-              </button>
-              <button class:selected={tab === 'Governor'} on:click={() => tab = 'Governor'}>
-                Governor
-              </button>
-              <button class:selected={tab === 'Custom'} on:click={() => tab = 'Custom'}>
-                Custom
-              </button>
-            </OverflowMenu>
+  <div class="flex flex-col gap-4 p-4">
+
+
+    <div class="flex flex-row justify-between">
+      <div class="overflow-hidden">
+
+            <ul class="menu menu-horizontal bg-base-200">
+              <li>
+                <button class:selected={tab === 'ERC20'} on:click={() => tab = 'ERC20'}>
+                  ERC20
+                </button>
+              </li>
+              <li>
+                <button class:selected={tab === 'ERC721'} on:click={() => tab = 'ERC721'}>
+                  ERC721
+                </button>
+              </li>
+              <li>
+                <button class:selected={tab === 'ERC1155'} on:click={() => tab = 'ERC1155'}>
+                  ERC1155
+                </button>
+              </li>
+              <li>
+                <button class:selected={tab === 'Governor'} on:click={() => tab = 'Governor'}>
+                  Governor
+                </button>
+              </li>
+              <li>
+                <button class:selected={tab === 'Custom'} on:click={() => tab = 'Custom'}>
+                  Custom
+                </button>
+              </li>
+            </ul>
+            
+      </div>
+
+      <div class="action flex flex-row gap-2 shrink-0">
+
+        <button class="action-button min-w-[165px]" on:click={copyHandler}>
+
+          <div class="flex justify-between">
+            {#if copied}
+              <CheckIcon />Copied
+            {:else}
+              <CopyIcon />Copy to Clipboard
+            {/if}
+
           </div>
+        </button>
+
+    
+      </div>
     </div>
 
   </div>
 
   <!-- <Footer links={footLinks}></Footer> -->
 
-<!-- <style lang="postcss">
-  .container {
+  <!-- <style lang="postcss">
+    .container {
+      background-color: var(--gray-1);
+      border: 1px solid var(--gray-2);
+      border-radius: 10px;
+      min-width: 32rem;
+    }
+  
+    .header {
+      font-size: var(--text-small);
+    }
+  
+    .tab {
+      color: var(--gray-5);
+    }
+  
+    /* .tab button, .action-button, :global(.overflow-btn) {
+      padding: var(--size-2) var(--size-3);
+      border-radius: 6px;
+      font-weight: bold;
+      cursor: pointer;
+    } */
+  
+    .tab button, :global(.overflow-btn) {
+      border: 0;
+      background-color: transparent;
+    }
+  
+    .tab button:hover, :global(.overflow-btn):hover {
+      background-color: var(--gray-2);
+    }
+  
+    .tab button.selected {
+      background-color: var(--solidity-blue-2);
+      color: white;
+      order: -1;
+    }
+  
+    :global(.overflow-menu) button.selected {
+      order: unset;
+    }
+
+    .action-button {
     background-color: var(--gray-1);
-    border: 1px solid var(--gray-2);
-    border-radius: 10px;
-    min-width: 32rem;
-  }
+    border: 1px solid var(--gray-3);
+    color: var(--gray-6);
+    cursor: pointer;
 
-  .header {
-    font-size: var(--text-small);
-  }
+    &:hover {
+      background-color: var(--gray-2);
+    }
 
-</style> -->
+    &:active, &.active {
+      background-color: var(--gray-2);
+    }
+
+    &.disabled {
+      color: var(--gray-4);
+    }
+
+    :global(.icon) {
+      margin-right: var(--size-1);
+    }
+  }
+  
+  </style> -->
+  
