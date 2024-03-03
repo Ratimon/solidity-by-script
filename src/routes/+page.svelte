50abb3c1a17e626115f9a986661384e4d9553177
Ratimon <ratixoxo@gmail.com>
2024-03-03 13:12:51 +0700

    Feat (./src/routes): Adding highlighted code component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,91 +1,89 @@
 <script  lang="ts">
 
-    import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher } from 'svelte';
 
-    import hljs from './highlightjs';
+  import Header from './Header.svelte';
+  import CopyIcon from '$lib/ui/icons/CopyIcon.svelte';
+  import CheckIcon from '$lib/ui/icons/CheckIcon.svelte';
 
-    import Header from './Header.svelte';
-    import CopyIcon from '$lib/ui/icons/CopyIcon.svelte';
-    import CheckIcon from '$lib/ui/icons/CheckIcon.svelte';
+  import ERC20Controls from '$lib/ui/controls/ERC20Controls.svelte';
+  import ERC721Controls from '$lib/ui/controls/ERC721Controls.svelte';
+  import ERC1155Controls from '$lib/ui/controls/ERC1155Controls.svelte';
+  import GovernorControls from '$lib/ui/controls/GovernorControls.svelte';
+  import CustomControls from '$lib/ui/controls/CustomControls.svelte';
+  // import Footer from './Footer.svelte';
 
-    import ERC20Controls from '$lib/ui/controls/ERC20Controls.svelte';
-    import ERC721Controls from '$lib/ui/controls/ERC721Controls.svelte';
-    import ERC1155Controls from '$lib/ui/controls/ERC1155Controls.svelte';
-    import GovernorControls from '$lib/ui/controls/GovernorControls.svelte';
-    import CustomControls from '$lib/ui/controls/CustomControls.svelte';
-    // import Footer from './Footer.svelte';
+  import { injectHyperlinks } from '$lib/ui/utils/inject-hyperlinks';
 
-    import type { KindedOptions, Kind, Contract, OptionsErrorMessages } from '$lib/wizard';
-    import { ContractBuilder, buildGeneric, printContract, sanitizeKind, OptionsError } from '$lib/wizard';
-    import { postConfig } from './post-config';
+  import type { KindedOptions, Kind, Contract, OptionsErrorMessages } from '$lib/wizard';
+  import { ContractBuilder, buildGeneric, printContract, sanitizeKind, OptionsError } from '$lib/wizard';
+  import hljs from './highlightjs';
+  import { postConfig } from './post-config';
 
-    import { injectHyperlinks } from '$lib/ui/utils/inject-hyperlinks';
+  // in case you want to use the multiple pages app 
+  const headLinks = [
+      {pathname: '/', title: 'Home'},
+      {pathname: '/docs', title: 'Documentation'},
+      {pathname: '/github', title: 'Github'},
+      {pathname: '/twitter', title: 'Twitter'},
+  ];
 
-    // in case you want to use the multiple pages app 
-    const headLinks = [
-        {pathname: '/', title: 'Home'},
-        {pathname: '/docs', title: 'Documentation'},
-        {pathname: '/github', title: 'github'},
-        {pathname: '/twitter', title: 'Twitter'},
-    ];
+  // const footLinks = [
+  //     {pathname: '/', title: 'Home'},
+  //     {pathname: '/docs', title: 'Documentation'},
+  //     {pathname: '/github', title: 'github'},
+  //     {pathname: '/twitter', title: 'Twitter'},
+  // ];
 
-    // const footLinks = [
-    //     {pathname: '/', title: 'Home'},
-    //     {pathname: '/docs', title: 'Documentation'},
-    //     {pathname: '/github', title: 'github'},
-    //     {pathname: '/twitter', title: 'Twitter'},
-  	// ];
+  const dispatch = createEventDispatcher();
 
-    const dispatch = createEventDispatcher();
+  export let initialTab: string | undefined = 'ERC20';
+  export let tab: Kind = sanitizeKind(initialTab);
 
-    export let initialTab: string | undefined = 'ERC20';
-    export let tab: Kind = sanitizeKind(initialTab);
+  $: {
+    tab = sanitizeKind(tab);
+    dispatch('tab-change', tab);
+  };
 
-    $: {
-      tab = sanitizeKind(tab);
-      dispatch('tab-change', tab);
-    };
+  let allOpts: { [k in Kind]?: Required<KindedOptions[k]> } = {};
+  let errors: { [k in Kind]?: OptionsErrorMessages } = {};
 
-    let allOpts: { [k in Kind]?: Required<KindedOptions[k]> } = {};
-    let errors: { [k in Kind]?: OptionsErrorMessages } = {};
+  let contract: Contract = new ContractBuilder('MyToken');
 
-    let contract: Contract = new ContractBuilder('MyToken');
+  $: opts = allOpts[tab];
 
-    $: opts = allOpts[tab];
-
-    $: {
-      if (opts) {
-        try {
-          contract = buildGeneric(opts);
-          errors[tab] = undefined;
-        } catch (e: unknown) {
-          if (e instanceof OptionsError) {
-            errors[tab] = e.messages;
-          } else {
-            throw e;
-          }
+  $: {
+    if (opts) {
+      try {
+        contract = buildGeneric(opts);
+        errors[tab] = undefined;
+      } catch (e: unknown) {
+        if (e instanceof OptionsError) {
+          errors[tab] = e.messages;
+        } else {
+          throw e;
         }
       }
     }
+  }
 
-$: code = printContract(contract);
-$: highlightedCode = injectHyperlinks(hljs.highlight('solidity', code).value);
+  $: code = printContract(contract);
+  $: highlightedCode = injectHyperlinks(hljs.highlight('solidity', code).value);
 
-const language = 'solidity';
+  const language = 'solidity';
 
-
-    let copied = false;
-    const copyHandler = async () => {
-      await navigator.clipboard.writeText(code);
-      copied = true;
-      if (opts) {
-        await postConfig(opts, 'copy', language);
-      }
-      setTimeout(() => {
-        copied = false;
-      }, 1000);
-    };
-    
+  let copied = false;
+  const copyHandler = async () => {
+    await navigator.clipboard.writeText(code);
+    copied = true;
+    if (opts) {
+      await postConfig(opts, 'copy', language);
+    }
+    setTimeout(() => {
+      copied = false;
+    }, 1000);
+  };
+  
   
   </script>
     
@@ -139,6 +137,8 @@ const language = 'solidity';
     </div>
 
     <div class="flex flex-row gap-4 grow">
+
+      
       <div class="controls w-64 flex flex-col shrink-0 justify-between h-[calc(100vh-84px)] overflow-auto">
         <div class:hidden={tab !== 'ERC20'}>
           <ERC20Controls bind:opts={allOpts.ERC20} />
@@ -156,6 +156,17 @@ const language = 'solidity';
           <CustomControls bind:opts={allOpts.Custom} />
         </div>
       </div>
+
+
+      <div class="output flex flex-col grow overflow-auto h-[calc(100vh-84px)]">
+        <pre class="flex flex-col grow basis-0 overflow-auto">
+          <code class="hljs grow overflow-auto p-4">
+            {@html highlightedCode}
+          </code>
+        </pre>
+      </div>
+
+
     </div>
 
   </div>
@@ -231,10 +242,10 @@ const language = 'solidity';
       padding: var(--size-4);
     }
   
-    /* .controls, .output {
+    .controls, .output {
       border-radius: 5px;
       box-shadow: var(--shadow);
     }
-   */
+  
   </style>
   
